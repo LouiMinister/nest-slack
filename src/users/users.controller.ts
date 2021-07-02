53c9b3292da6b5e949e7cf1dcea48f5e690a3aa6
LouiMinister <louiminister@gmail.com>
2021-07-02 16:06:36 +0900

    feature: generate modules

---
+++ src/users/users.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  // @Get()
+  // getUsers(@Req() req) {
+  //   return req.user;
+  // }
+  // @Post()
+  // postUsers() {
+  // }
+  // @Post('login')
+  // logIn() { }
+  // @Post('logout')
+  // logOut(@Req() req) {}
+}
