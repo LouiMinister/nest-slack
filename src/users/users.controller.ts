6ea0a1299b26b1e204f8fe5f59d9cee79b6f77ec
LouiMinister <louiminister@gmail.com>
2021-07-03 16:00:14 +0900

    feature: 커스텀 인터셉터 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserDto } from 'src/common/dto/user.dto';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { User } from '../common/decorators/user.decorator';
 import { UsersService } from './users.service';
+import { UndefinedToNullInterceptor } from 'src/common/interceptors/undefinedToNull.interceptor';
 
+@UseInterceptors(UndefinedToNullInterceptor)
 @Controller('api/users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
