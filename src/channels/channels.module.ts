4f078189f53d27f72688779df7ccc1db77849d23
LouiMinister <louiminister@gmail.com>
2021-07-08 21:02:09 +0900

    feature: TypeORM 추가 및 configuration

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { WorkspacesService } from 'src/workspaces/workspaces.service';
 import { ChannelsController } from './channels.controller';
 
 @Module({
+  providers: [WorkspacesService],
   controllers: [ChannelsController],
 })
 export class ChannelsModule {}
