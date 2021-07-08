f091fb9db1ee91d9c1ea237fd63afa09f9b84a17
LouiMinister <louiminister@gmail.com>
2021-07-08 22:19:49 +0900

    fix: Mapped types 이용한 DTO 중복 제거

--- src/users/dto/join.request.dto.ts
+++ src/users/dto/join.request.dto.ts
@@ -1,24 +1,8 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
+import { Users } from 'src/entities/Users';
 
-export class JoinRequestDto {
-  @ApiProperty({
-    example: 'asdf@gmail.com',
-    description: '이메일',
-    required: true,
-  })
-  public email: string;
-
-  @ApiProperty({
-    example: '고라니',
-    description: '닉네임',
-    required: true,
-  })
-  public nickname: string;
-
-  @ApiProperty({
-    example: 'asd1234',
-    description: 'password',
-    required: true,
-  })
-  public password: string;
-}
+export class JoinRequestDto extends PickType(Users, [
+  'email',
+  'nickname',
+  'password',
+] as const) {}
